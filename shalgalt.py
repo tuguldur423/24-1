4a722121617070bebd7a1e49d88569d60970f23d
tuguldur423 <buyndelgertuguldur@gmail.com>
2024-10-25 16:25:45 +0800

    Update shalgalt.py

--- shalgalt.py
+++ shalgalt.py
@@ -23,7 +23,6 @@ def huffman_code(frequencies):
         merged.right = right
         heap.heappush(heap, merged)
 
-    # Кодын үүсгэх
     root = heap[0]
     huffman_codes = {}
     
@@ -38,7 +37,6 @@ def huffman_code(frequencies):
 
     return huffman_codes
 
-# Жишээ өгөгдөл
 frequencies = {
     'A': 5,
     'B': 9,
@@ -48,8 +46,60 @@ frequencies = {
     'F': 45
 }
 
-# Хаффман кодыг олох
 huffman_codes = huffman_code(frequencies)
+
+# Кодыг хэвлэх
+print("Хаффман код:")
+for char, code in huffman_codes.items():
+    print(f"{char}: {code}")
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+arr = [64,34,25,12,22,11,90]
+bubble_sort(arr)
+print("Sorted array:", arr)
+
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+arr = [64,34,25,12,22,11,90]
+insertion_sort(arr)
+print("Sort", arr)
+
+# 1=b
+# 2=b
+# 3=c
+# 4=a
+# 5=c
+# 6=a
+# 7=c
+# 7-1=c
+# 8=c
+# 9=a
+# 10=d
+# 11=b
+# 12=b
+# 13=
+# 14=
+# 15=c
+# 16=a
+# 17=b
+# 18=c
+# asuult2=b
+# algoritminguitsetgel=b
+# punkts butsaah=b
 
 # Кодыг хэвлэх
 print("Хаффман код:")
